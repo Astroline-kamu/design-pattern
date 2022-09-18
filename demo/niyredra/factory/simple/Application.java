3e945057d977886d3c50e897033fc12931961b23
Astroline <Astroline_kamu@outlook.com>
2022-09-18 14:59:44 +0800

    简单工厂模式 --测试Readme跳转

---
+++ demo/niyredra/factory/simple/Application.java
@@ -0,0 +1,15 @@
+package niyredra.factory.simple;
+
+/**
+ *
+ * 简单工厂模式
+ *
+ * @author Niyredra Astroline_kamu@outlook.com
+ */
+public class Application {
+
+
+    public static void main(String[] args) {
+
+    }
+}
